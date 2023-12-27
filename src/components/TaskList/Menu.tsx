d62663a50c302212647a62a744f93fb45bce0c94
parkcoool <yickle1345@naver.com>
2023-12-27 22:06:56 +0900

    feat: Add styles for TaskItem menu

--- src/components/TaskList/Menu.tsx
+++ src/components/TaskList/Menu.tsx
@@ -1,11 +1,24 @@
 import styles from "./styles.module.css";
+import deleteIcon from "../../assets/delete.svg";
+import editIcon from "../../assets/edit.svg";
 
 interface MenuProps {
     task: Task;
 }
 
 function Menu({ task }: MenuProps) {
-    return <div></div>;
+    return (
+        <div className={styles.menu}>
+            <button className={styles.menuButton}>
+                <img src={deleteIcon} alt="삭제"></img>
+                <span>삭제</span>
+            </button>
+            <button className={styles.menuButton}>
+                <img src={editIcon} alt="편집"></img>
+                <span>편집</span>
+            </button>
+        </div>
+    );
 }
 
 export default Menu;
