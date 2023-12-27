6be0196343125dc61174f952fe6c7417a1da482d
parkcoool <yickle1345@naver.com>
2023-12-27 19:13:01 +0900

    feat: Add buttons in TaskItem

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,4 +1,6 @@
 import styles from "./styles.module.css";
+import editIcon from "../../assets/edit.svg";
+import deleteIcon from "../../assets/delete.svg";
 
 interface TaskItemProps {
     task: Task;
@@ -20,10 +22,20 @@ function elapsedTime(date: string) {
 function TaskItem({ task }: TaskItemProps) {
     return (
         <div className={styles.task}>
-            <div className={styles.done}>{task.done ? "✅" : "⬜"}</div>
-            <div className={styles.info}>
-                <h1>{task.name}</h1>
-                <h2>{elapsedTime(task.date)}</h2>
+            <div className={styles.left}>
+                <div className={styles.done}>{task.done ? "✅" : "⬜"}</div>
+                <div className={styles.info}>
+                    <h1>{task.name}</h1>
+                    <h2>{elapsedTime(task.date)}</h2>
+                </div>
+            </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.imgButton}>
+                    <img src={editIcon} alt="edit"></img>
+                </button>
+                <button className={styles.imgButton}>
+                    <img src={deleteIcon} alt="delete"></img>
+                </button>
             </div>
         </div>
     );
